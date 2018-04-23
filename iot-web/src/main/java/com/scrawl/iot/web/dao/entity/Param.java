cce058c6e5a50b7313c51044ee3bce06612e2fdd
as <88452486@qq.com>
2018-04-23 19:18:42 +0800

    sys param

---
+++ iot-web/src/main/java/com/scrawl/iot/web/dao/entity/Param.java
@@ -0,0 +1,75 @@
+package com.scrawl.iot.web.dao.entity;
+
+import java.util.Date;
+
+public class Param {
+    private Integer id;
+
+    private String paramKey;
+
+    private String paramValue;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Integer createManager;
+
+    private Integer updateManager;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getParamKey() {
+        return paramKey;
+    }
+
+    public void setParamKey(String paramKey) {
+        this.paramKey = paramKey;
+    }
+
+    public String getParamValue() {
+        return paramValue;
+    }
+
+    public void setParamValue(String paramValue) {
+        this.paramValue = paramValue;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getCreateManager() {
+        return createManager;
+    }
+
+    public void setCreateManager(Integer createManager) {
+        this.createManager = createManager;
+    }
+
+    public Integer getUpdateManager() {
+        return updateManager;
+    }
+
+    public void setUpdateManager(Integer updateManager) {
+        this.updateManager = updateManager;
+    }
+}
