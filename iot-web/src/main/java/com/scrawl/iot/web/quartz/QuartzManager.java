576bb66b329b9cc5683c7a8319d9d98c7e0ab310
as <88452486@qq.com>
2018-04-29 16:57:57 +0800

    job

---
+++ iot-web/src/main/java/com/scrawl/iot/web/quartz/QuartzManager.java
@@ -0,0 +1,177 @@
+package com.scrawl.iot.web.quartz;
+
+import com.scrawl.iot.web.dao.entity.Task;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.quartz.DateBuilder.IntervalUnit;
+import org.quartz.impl.matchers.GroupMatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @title: QuartzManager.java
+ * @description: 计划任务管理
+ */
+@Service
+@Slf4j
+public class QuartzManager {
+
+    @Autowired
+    private Scheduler scheduler;
+
+    /**
+     * 添加任务
+     *
+     * @param job
+     */
+    public void addJob(Task job) {
+        try {
+            // 创建jobDetail实例，绑定Job实现类
+            // 指明job的名称，所在组的名称，以及绑定job类
+
+            Class<? extends Job> jobClass = (Class<? extends Job>) (Class.forName(job.getBeanClass()).newInstance().getClass());
+            JobDetail jobDetail = JobBuilder.newJob(jobClass).withIdentity(job.getJobName(), job.getJobGroup())// 任务名称和组构成任务key
+                    .build();
+            // 定义调度触发规则
+            // 使用cornTrigger规则
+            Trigger trigger = TriggerBuilder.newTrigger().withIdentity(job.getJobName(), job.getJobGroup())// 触发器key
+                    .startAt(DateBuilder.futureDate(1, IntervalUnit.SECOND))
+                    .withSchedule(CronScheduleBuilder.cronSchedule(job.getCronExpression())).startNow().build();
+            // 把作业和触发器注册到任务调度中
+            scheduler.scheduleJob(jobDetail, trigger);
+            // 启动
+            if (!scheduler.isShutdown()) {
+                scheduler.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取所有计划中的任务列表
+     *
+     * @return task列表
+     * @throws SchedulerException 调度任务异常
+     */
+    public List<Task> getAllJob() throws SchedulerException {
+        GroupMatcher<JobKey> matcher = GroupMatcher.anyJobGroup();
+        Set<JobKey> jobKeys = scheduler.getJobKeys(matcher);
+        List<Task> jobList = new ArrayList<>();
+        for (JobKey jobKey : jobKeys) {
+            List<? extends Trigger> triggers = scheduler.getTriggersOfJob(jobKey);
+            for (Trigger trigger : triggers) {
+                Task job = new Task();
+                job.setJobName(jobKey.getName());
+                job.setJobGroup(jobKey.getGroup());
+                job.setDescription("触发器:" + trigger.getKey());
+                Trigger.TriggerState triggerState = scheduler.getTriggerState(trigger.getKey());
+                job.setJobStatus(triggerState.name());
+                if (trigger instanceof CronTrigger) {
+                    CronTrigger cronTrigger = (CronTrigger) trigger;
+                    String cronExpression = cronTrigger.getCronExpression();
+                    job.setCronExpression(cronExpression);
+                }
+                jobList.add(job);
+            }
+        }
+        return jobList;
+    }
+
+    /**
+     * 所有正在运行的job
+     *
+     * @return task列表
+     * @throws SchedulerException 调度任务异常
+     */
+    public List<Task> getRunningJob() throws SchedulerException {
+        List<JobExecutionContext> executingJobs = scheduler.getCurrentlyExecutingJobs();
+        List<Task> jobList = new ArrayList<>(executingJobs.size());
+        for (JobExecutionContext executingJob : executingJobs) {
+            Task job = new Task();
+            JobDetail jobDetail = executingJob.getJobDetail();
+            JobKey jobKey = jobDetail.getKey();
+            Trigger trigger = executingJob.getTrigger();
+            job.setJobName(jobKey.getName());
+            job.setJobGroup(jobKey.getGroup());
+            job.setDescription("触发器:" + trigger.getKey());
+            Trigger.TriggerState triggerState = scheduler.getTriggerState(trigger.getKey());
+            job.setJobStatus(triggerState.name());
+            if (trigger instanceof CronTrigger) {
+                CronTrigger cronTrigger = (CronTrigger) trigger;
+                String cronExpression = cronTrigger.getCronExpression();
+                job.setCronExpression(cronExpression);
+            }
+            jobList.add(job);
+        }
+        return jobList;
+    }
+
+    /**
+     * 暂停一个job
+     *
+     * @param scheduleJob job
+     * @throws SchedulerException 调度任务异常
+     */
+    public void pauseJob(Task scheduleJob) throws SchedulerException {
+        JobKey jobKey = JobKey.jobKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
+        scheduler.pauseJob(jobKey);
+    }
+
+    /**
+     * 恢复一个job
+     *
+     * @param scheduleJob job
+     * @throws SchedulerException 调度任务异常
+     */
+    public void resumeJob(Task scheduleJob) throws SchedulerException {
+        JobKey jobKey = JobKey.jobKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
+        scheduler.resumeJob(jobKey);
+    }
+
+    /**
+     * 删除一个job
+     *
+     * @param scheduleJob job
+     * @throws SchedulerException 调度任务异常
+     */
+    public void deleteJob(Task scheduleJob) throws SchedulerException {
+        JobKey jobKey = JobKey.jobKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
+        scheduler.deleteJob(jobKey);
+
+    }
+
+    /**
+     * 立即执行job
+     *
+     * @param scheduleJob job
+     * @throws SchedulerException 调度任务异常
+     */
+    public void runAJobNow(Task scheduleJob) throws SchedulerException {
+        JobKey jobKey = JobKey.jobKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
+        scheduler.triggerJob(jobKey);
+    }
+
+    /**
+     * 更新job时间表达式
+     *
+     * @param scheduleJob job
+     * @throws SchedulerException 调度任务异常
+     */
+    public void updateJobCron(Task scheduleJob) throws SchedulerException {
+
+        TriggerKey triggerKey = TriggerKey.triggerKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
+
+        CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
+
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(scheduleJob.getCronExpression());
+
+        trigger = trigger.getTriggerBuilder().withIdentity(triggerKey).withSchedule(scheduleBuilder).build();
+
+        scheduler.rescheduleJob(triggerKey, trigger);
+    }
+}
---
+++ iot-web/src/main/java/com/scrawl/iot/web/quartz/ScheduleJob.java
@@ -0,0 +1,71 @@
+package com.scrawl.iot.web.quartz;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+public class ScheduleJob implements Serializable, Job {
+
+    private static final long serialVersionUID = -7022463383502762171L;
+
+    public static final String STATUS_RUNNING = "1";
+    public static final String STATUS_NOT_RUNNING = "0";
+    public static final String CONCURRENT_IS = "1";
+    public static final String CONCURRENT_NOT = "0";
+
+    /**
+     * 任务名称
+     */
+    private String jobName;
+
+    /**
+     * 任务分组
+     */
+    private String jobGroup;
+
+    /**
+     * 任务状态 是否启动任务
+     */
+    private String jobStatus;
+
+    /**
+     * cron表达式
+     */
+    private String cronExpression;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 任务执行时调用哪个类的方法 包名+类名
+     */
+    private String beanClass;
+
+    /**
+     * 任务是否有状态
+     */
+    private String isConcurrent;
+
+    /**
+     * Spring bean
+     */
+    private String springBean;
+
+    /**
+     * 任务调用的方法名
+     */
+    private String methodName;
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        // TODO Auto-generated method stub
+    }
+}
